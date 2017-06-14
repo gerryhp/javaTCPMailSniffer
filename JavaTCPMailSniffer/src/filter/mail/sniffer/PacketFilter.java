aac6b86226a240f9c63b82f86f80359a590cde47
obalkad <obalkad@hochschule-trier.de>
2017-06-14 17:48:15 +0200

    Update PacketFilter.java

--- JavaTCPMailSniffer/src/filter/mail/sniffer/PacketFilter.java
+++ JavaTCPMailSniffer/src/filter/mail/sniffer/PacketFilter.java
@@ -1,5 +1,64 @@
 package filter.mail.sniffer;
 
-public class PacketFilter {
+import java.util.ArrayList;
+import javax.mail.Session;
 
+public class PacketFilter {
+	
+	private int protcolSelected;
+	private String searchExpr;
+	private ArrayList<Session> matches;
+	
+	/**
+	 * Three valid Email Protocol Ports to sniff, other protocols are not supported
+	 */
+	public final int POP3 = 110;
+	public final int IMAP = 143;
+	public final int SMTP = 25;
+	
+	public PacketFilter(int protcolSelected) {
+		super();
+		this.protcolSelected = protcolSelected;
+		this.searchExpr = "";
+		this.matches = new ArrayList<>();
+	}
+
+	public int getProtcolSelected() {
+		return protcolSelected;
+	}
+
+	public void setProtcolSelected(int protcolSelected) {
+		this.protcolSelected = protcolSelected;
+		resetMatches();
+	}
+
+	public String getSearchExpr() {
+		return searchExpr;
+	}
+
+	public void setSearchExpr(String searchExpr) {
+		this.searchExpr = searchExpr;
+		resetMatches();
+	}
+	
+	/**
+	 * Removes all old matches, which is necessary for a new searchExpr  
+	 */
+	private void resetMatches() {
+		matches.clear();
+	}
+	/**
+	 * This is the filter function for any of the three protocols.
+	 * Session is the most important Class for Email in Java
+	 * Once the Socket extracts some Mail Traffic, it needs
+	 * to create a new Session out of the collected informations.
+	 * This method checks, if any session contains the @param searchExpr. 
+	 */
+	public ArrayList<Session> filter(/*needs to be discussed*/) {
+		if (searchExpr.equals("")) {
+			return null;
+		}
+		// to do ...
+		return matches;
+	}	
 }

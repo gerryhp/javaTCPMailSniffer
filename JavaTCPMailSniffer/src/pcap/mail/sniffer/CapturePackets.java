4d8b696cc3830456611be077b0f8b5c79ae3c060
Gerry <Gerry@Gerry-PC.fritz.box>
2017-06-18 14:00:18 +0200

    added to start capturing on the network interface

--- JavaTCPMailSniffer/src/pcap/mail/sniffer/CapturePackets.java
+++ JavaTCPMailSniffer/src/pcap/mail/sniffer/CapturePackets.java
@@ -6,37 +6,7 @@ import org.jnetpcap.PcapIf;
 
 public class CapturePackets {
 	
-	/**
-	 * starts to capture packets
-	 */
-	public void capture() {
-		int snaplen = 64*1024; //capture all packets, no trucation
-		int flags = Pcap.MODE_PROMISCUOUS; //capture all packets;
-		int timeout = 10 * 1000; //timeout in milli
-		
-		ArrayList<PcapIf> alldevs = new NetworkInterfaces().getDevices();
-		StringBuilder errbuf = new StringBuilder();
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				Pcap pcap = Pcap.openLive(alldevs.get(2).getName(), snaplen, flags, timeout, errbuf);
-				
-				if (pcap == null) {
-					System.err.println("Error");
-				}
-								
-				Packets packets = new Packets(true);
-				
-				/*
-				 * captures every packet
-				 */
-				pcap.loop(-1, packets, "jNetPcap");					
-				pcap.close();
-			}
-		}).start();
-	}
+	private static final int SNAPLEN = 64*1024; //capture all packets, no trucation
 	
 	public void capture(PcapIf pcapIf) {
 		
